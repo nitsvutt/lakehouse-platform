6823c067e12e927eabfc46a1d7b953f6294d5826
nitsvutt <nitsvutt@gmail.com>
2024-04-27 10:44:16 +0700

    config file

---
+++ workspace/mockup-data/app/product2/core/config.py
@@ -0,0 +1,7 @@
+import os
+
+# mysql connection
+MYSQL_SERVER = os.environ['MYSQL_SERVER']
+MYSQL_PORT = os.environ['MYSQL_PORT']
+MYSQL_USER = os.environ['MYSQL_USER']
+MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
