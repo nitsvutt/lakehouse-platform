3efe1865cc7e4d972e085e06ac815439290dafae
nitsvutt <nitsvutt@gmail.com>
2024-04-30 09:26:15 +0700

    update period table

--- data-source/postgres.sql
+++ data-source/postgres.sql
@@ -33,8 +33,7 @@ create table trans(
 	trans_id bigint default nextval('trans_id_serial') primary key,
 	customer_id bigint,
 	service_id bigint,
-	period_type_id int,
-	amount decimal(38,5),
+	period_id int,
 	review_score int,
 	review_comment varchar(99),
 	review_images varchar(99),
@@ -42,10 +41,12 @@ create table trans(
 	updated_datetime timestamp
 );
 
-create sequence period_type_id_serial start 1;
-create table period_type(
-	period_type_id int default nextval('period_type_id_serial') primary key,
-	period_type varchar(99),
+create sequence period_id_serial start 1;
+create table period(
+	period_id int default nextval('period_id_serial') primary key,
+	name varchar(99),
+	factor decimal(38,5),
+	extra decimal(38,5),
 	active_status int,
 	created_datetime timestamp,
 	updated_datetime timestamp
