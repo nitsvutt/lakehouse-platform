23efc58e9680d13ceed379b6d534e0bd30f5181c
nitsvutt <nitsvutt@gmail.com>
2024-04-30 13:37:36 +0700

    add active and inactive date to customer model

--- workspace/mockup-data/app/product1/db/model/customer.py
+++ workspace/mockup-data/app/product1/db/model/customer.py
@@ -4,7 +4,7 @@ from db.session import Base
 class Customer(Base):
     __tablename__ = "customer"
 
-    customer_id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer)
     first_name = Column(String)
     last_name = Column(String)
     birth_date = Column(Date)
@@ -13,6 +13,8 @@ class Customer(Base):
     email = Column(String)
     job_title = Column(String)
     active_status = Column(Integer)
+    active_date = Column(Date)
+    inactive_date = Column(Date)
     created_datetime = Column(DateTime)
     updated_datetime = Column(DateTime)
 
