9cf3bb0a846e4368268bb9bf526aa31d78746a4a
nitsvutt <nitsvutt@gmail.com>
2024-04-30 13:35:21 +0700

    update postgres tables

--- data-source/postgres.sql
+++ data-source/postgres.sql
@@ -4,7 +4,7 @@ create database product1;
 -- create table
 create sequence customer_id_serial start 1;
 create table customer(
-	customer_id bigint default nextval('customer_id_serial') primary key,
+	customer_id bigint default nextval('customer_id_serial'),
 	first_name varchar(99),
 	last_name varchar(99),
 	birth_date date,
@@ -13,41 +13,57 @@ create table customer(
 	email varchar(99),
 	job_title varchar(99),
 	active_status int,
+	active_date date,
+	inactive_date date,
 	created_datetime timestamp,
 	updated_datetime timestamp
 );
 
 create sequence service_id_serial start 1;
 create table service(
-	service_id bigint default nextval('service_id_serial') primary key,
+	service_id bigint default nextval('service_id_serial'),
 	name varchar(99),
 	price decimal(38,5),
 	image varchar(99),
 	active_status int,
-	created_datetime timestamp,
-	updated_datetime timestamp
-);
-
-create sequence trans_id_serial start 1;
-create table trans(
-	trans_id bigint default nextval('trans_id_serial') primary key,
-	customer_id bigint,
-	service_id bigint,
-	period_id int,
-	review_score int,
-	review_comment varchar(99),
-	review_images varchar(99),
+	active_date date,
+	inactive_date date,
 	created_datetime timestamp,
 	updated_datetime timestamp
 );
 
 create sequence period_id_serial start 1;
 create table period(
-	period_id int default nextval('period_id_serial') primary key,
+	period_id int default nextval('period_id_serial'),
 	name varchar(99),
 	factor decimal(38,5),
 	extra decimal(38,5),
 	active_status int,
+	active_date date,
+	inactive_date date,
+	created_datetime timestamp,
+	updated_datetime timestamp
+);
+
+create sequence trans_id_serial start 1;
+create table trans(
+	trans_id bigint default nextval('trans_id_serial'),
+	customer_id bigint,
+	service_id bigint,
+	period_id int,
+	effect_date date,
+	created_datetime timestamp,
+	updated_datetime timestamp
+);
+
+create sequence review_id_serial start 1;
+create table review(
+	review_id bigint default nextval('review_id_serial'),
+	trans_id bigint,
+	score int,
+	comment varchar(99),
+	image varchar(99),
+	posted_datetime date,
 	created_datetime timestamp,
 	updated_datetime timestamp
 );
