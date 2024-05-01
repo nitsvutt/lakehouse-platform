f87afb1d544b3e175bfea043beebe64385b9eed9
nitsvutt <nitsvutt@gmail.com>
2024-05-01 17:49:33 +0700

    add active date and inactive date column to service schema

--- workspace/mockup-data/app/product1/db/schema/service.py
+++ workspace/mockup-data/app/product1/db/schema/service.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 from datetime import date
 
 class ServiceBase(BaseModel):
+    service_id: int | None = None
     name: str = None
     price: float = None
     image: str = None
     active_date: date = None
-    inactive_date: date = None
+    inactive_date: date | None = None
 
 class Service(ServiceBase):
-    service_id: int
 
     class Config:
         orm_mode = True
