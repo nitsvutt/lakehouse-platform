290546070be79bd83fc285cc849d6b50cd453483
nitsvutt <nitsvutt@gmail.com>
2024-05-03 23:39:22 +0700

    update service model remove created datetime

--- workspace/mockup-data/app/product1/db/model/service.py
+++ workspace/mockup-data/app/product1/db/model/service.py
@@ -5,25 +5,10 @@ from db.session import Base
 class Service(Base):
     __tablename__ = "service"
 
-    system_id = Column(Integer, primary_key=True, server_default=expression.text("True"))
-    service_id = Column(Integer, server_default=expression.text("True"))
+    service_id = Column(Integer, primary_key=True, server_default=expression.text("True"))
     name = Column(String)
     price = Column(Float)
     image = Column(String)
     active_date = Column(Date)
     inactive_date = Column(Date)
-    created_datetime = Column(DateTime)
-    updated_datetime = Column(DateTime)
-
-class ServiceHist(Base):
-    __tablename__ = "service_hist"
-
-    system_id = Column(Integer, primary_key=True, server_default=expression.text("True"))
-    service_id = Column(Integer)
-    name = Column(String)
-    price = Column(Float)
-    image = Column(String)
-    active_date = Column(Date)
-    inactive_date = Column(Date)
-    created_datetime = Column(DateTime)
     updated_datetime = Column(DateTime)
