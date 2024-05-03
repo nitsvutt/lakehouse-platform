071abb0b6fe89964848527dce11d34681ed33758
nitsvutt <nitsvutt@gmail.com>
2024-05-03 23:40:08 +0700

    update service crud remove hist

--- workspace/mockup-data/app/product1/db/crud/service.py
+++ workspace/mockup-data/app/product1/db/crud/service.py
@@ -1,56 +1,42 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
-from datetime import date
+
 from db import model, schema
 from core.utils import current_sysdate, current_systime
 
 def create_service(service: schema.ServiceCreate, db: Session):
     db_service = (
         db.query(model.Service)
-        .filter(model.Service.service_id == service.service_id)
+        .filter(model.Service.name == service.name)
         .filter(model.Service.active_date == service.active_date)
         .first()
     )
     if db_service:
         raise HTTPException(
             status_code=409,
-            detail="Service exists"
+            detail="Service name and active date exists"
         )
     
     db_service = model.Service(
-        service_id = service.service_id,
         name = service.name,
         price = service.price,
         image = service.image,
         active_date = service.active_date,
         inactive_date = service.inactive_date,
-        created_datetime = current_systime()
+        updated_datetime = current_systime()
     )
     db.add(db_service)
     db.commit()
     db.refresh(db_service)
-
-    db_service_hist = model.ServiceHist(
-        service_id = db_service.service_id,
-        name = db_service.name,
-        price = db_service.price,
-        image = db_service.image,
-        active_date = db_service.active_date,
-        inactive_date = db_service.inactive_date,
-        created_datetime = db_service.created_datetime
-    )
-    db.add(db_service_hist)
-    db.commit()
-
     return db_service
 
 def select_all_services(db: Session):
     return db.query(model.Service).all()
 
-def select_service_by_id(system_id: int, db: Session):
+def select_service_by_id(service_id: int, db: Session):
     db_service = (
         db.query(model.Service)
-        .filter(model.Service.system_id == system_id)
+        .filter(model.Service.service_id == service_id)
         .first()
     )
     if not db_service:
@@ -73,10 +59,21 @@ def select_service_by_name(name: str, db: Session):
         )
     return db_service
 
-def update_service(system_id: int, service: schema.ServiceUpdate, db: Session):
+def update_service(service_id: int, service: schema.ServiceUpdate, db: Session):
     db_service = (
         db.query(model.Service)
-        .filter(model.Service.system_id == system_id)
+        .filter(model.Service.name == service.name)
+        .filter(model.Service.active_date == service.active_date)
+        .first()
+    )
+    if db_service:
+        raise HTTPException(
+            status_code=409,
+            detail="Service name and active date exists"
+        )
+    db_service = (
+        db.query(model.Service)
+        .filter(model.Service.service_id == service_id)
         .first()
     )
     if not db_service:
@@ -84,6 +81,7 @@ def update_service(system_id: int, service: schema.ServiceUpdate, db: Session):
             status_code=404,
             detail="Service not found"
         )
+    
     update_data = service.dict()
     for key, value in update_data.items():
         if value is not None:
@@ -92,26 +90,12 @@ def update_service(system_id: int, service: schema.ServiceUpdate, db: Session):
     db.add(db_service)
     db.commit()
     db.refresh(db_service)
-
-    db_service_hist = model.ServiceHist(
-        service_id = db_service.service_id,
-        name = db_service.name,
-        price = db_service.price,
-        image = db_service.image,
-        active_date = db_service.active_date,
-        inactive_date = db_service.inactive_date,
-        created_datetime = db_service.created_datetime,
-        updated_datetime = db_service.updated_datetime
-    )
-    db.add(db_service_hist)
-    db.commit()
-
     return db_service
 
-def delete_service(system_id: int, db: Session):
+def delete_service(service_id: int, db: Session):
     db_service = (
         db.query(model.Service)
-        .filter(model.Service.system_id == system_id)
+        .filter(model.Service.service_id == service_id)
         .first()
     )
     if not db_service:
@@ -121,18 +105,4 @@ def delete_service(system_id: int, db: Session):
         )
     db.delete(db_service)
     db.commit()
-
-    db_service_hist = model.ServiceHist(
-        service_id = db_service.service_id,
-        name = db_service.name,
-        price = db_service.price,
-        image = db_service.image,
-        active_date = db_service.active_date,
-        inactive_date = db_service.inactive_date,
-        created_datetime = db_service.created_datetime,
-        updated_datetime = current_systime()
-    )
-    db.add(db_service_hist)
-    db.commit()
-
     return db_service
