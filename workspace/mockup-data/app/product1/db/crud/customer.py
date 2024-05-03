12af066e9529fa2a705fa80c4af19f95633e76d7
nitsvutt <nitsvutt@gmail.com>
2024-05-03 23:09:48 +0700

    update customer crud remove hist

--- workspace/mockup-data/app/product1/db/crud/customer.py
+++ workspace/mockup-data/app/product1/db/crud/customer.py
@@ -7,18 +7,26 @@ from core.utils import current_sysdate, current_systime
 def create_customer(customer: schema.CustomerCreate, db: Session):
     db_customer = (
         db.query(model.Customer)
-        .filter(model.Customer.customer_id == customer.customer_id)
-        .filter(model.Customer.active_date == customer.active_date)
+        .filter(model.Customer.phone_number == customer.phone_number)
         .first()
     )
     if db_customer:
         raise HTTPException(
             status_code=409,
-            detail="Customer exists"
+            detail="Phone number exists"
+        )
+    db_customer = (
+        db.query(model.Customer)
+        .filter(model.Customer.email == customer.email)
+        .first()
+    )
+    if db_customer:
+        raise HTTPException(
+            status_code=409,
+            detail="Email exists"
         )
     
     db_customer = model.Customer(
-        customer_id = customer.customer_id,
         first_name = customer.first_name,
         last_name = customer.last_name,
         birth_date = customer.birth_date,
@@ -26,39 +34,20 @@ def create_customer(customer: schema.CustomerCreate, db: Session):
         phone_number = customer.phone_number,
         email = customer.email,
         job_title = customer.job_title,
-        active_date = customer.active_date,
-        inactive_date = customer.inactive_date,
-        created_datetime = current_systime()
+        updated_datetime = current_systime()
     )
     db.add(db_customer)
     db.commit()
     db.refresh(db_customer)
-
-    db_customer_hist = model.CustomerHist(
-        customer_id = db_customer.customer_id,
-        first_name = db_customer.first_name,
-        last_name = db_customer.last_name,
-        birth_date = db_customer.birth_date,
-        address = db_customer.address,
-        phone_number = db_customer.phone_number,
-        email = db_customer.email,
-        job_title = db_customer.job_title,
-        active_date = db_customer.active_date,
-        inactive_date = db_customer.inactive_date,
-        created_datetime = db_customer.created_datetime
-    )
-    db.add(db_customer_hist)
-    db.commit()
-
     return db_customer
 
 def select_all_customers(db: Session):
     return db.query(model.Customer).all()
 
-def select_customer_by_id(system_id: int, db: Session):
+def select_customer_by_id(customer_id: int, db: Session):
     db_customer = (
         db.query(model.Customer)
-        .filter(model.Customer.system_id == system_id)
+        .filter(model.Customer.customer_id == customer_id)
         .first()
     )
     if not db_customer:
@@ -85,7 +74,7 @@ def select_customer_by_phone_number(phone_number: str, db: Session):
     db_customer = (
         db.query(model.Customer)
         .filter(model.Customer.phone_number == phone_number)
-        .all()
+        .first()
     )
     if not db_customer:
         raise HTTPException(
@@ -98,7 +87,7 @@ def select_customer_by_email(email: str, db: Session):
     db_customer = (
         db.query(model.Customer)
         .filter(model.Customer.email == email)
-        .all()
+        .first()
     )
     if not db_customer:
         raise HTTPException(
@@ -107,10 +96,30 @@ def select_customer_by_email(email: str, db: Session):
         )
     return db_customer
 
-def update_customer(system_id: int, customer: schema.CustomerUpdate, db: Session):
+def update_customer(customer_id: int, customer: schema.CustomerUpdate, db: Session):
     db_customer = (
         db.query(model.Customer)
-        .filter(model.Customer.system_id == system_id)
+        .filter(model.Customer.phone_number == customer.phone_number)
+        .first()
+    )
+    if db_customer:
+        raise HTTPException(
+            status_code=409,
+            detail="Phone number exists"
+        )
+    db_customer = (
+        db.query(model.Customer)
+        .filter(model.Customer.email == customer.email)
+        .first()
+    )
+    if db_customer:
+        raise HTTPException(
+            status_code=409,
+            detail="Email exists"
+        )
+    db_customer = (
+        db.query(model.Customer)
+        .filter(model.Customer.customer_id == customer_id)
         .first()
     )
     if not db_customer:
@@ -118,6 +127,7 @@ def update_customer(system_id: int, customer: schema.CustomerUpdate, db: Session
             status_code=404,
             detail="Customer not found"
         )
+
     update_data = customer.dict()
     for key, value in update_data.items():
         if value is not None:
@@ -126,30 +136,12 @@ def update_customer(system_id: int, customer: schema.CustomerUpdate, db: Session
     db.add(db_customer)
     db.commit()
     db.refresh(db_customer)
-
-    db_customer_hist = model.CustomerHist(
-        customer_id = db_customer.customer_id,
-        first_name = db_customer.first_name,
-        last_name = db_customer.last_name,
-        birth_date = db_customer.birth_date,
-        address = db_customer.address,
-        phone_number = db_customer.phone_number,
-        email = db_customer.email,
-        job_title = db_customer.job_title,
-        active_date = db_customer.active_date,
-        inactive_date = db_customer.inactive_date,
-        created_datetime = db_customer.created_datetime,
-        updated_datetime = db_customer.updated_datetime
-    )
-    db.add(db_customer_hist)
-    db.commit()
-
     return db_customer
 
-def delete_customer(system_id: int, db: Session):
+def delete_customer(customer_id: int, db: Session):
     db_customer = (
         db.query(model.Customer)
-        .filter(model.Customer.system_id == system_id)
+        .filter(model.Customer.customer_id == customer_id)
         .first()
     )
     if not db_customer:
@@ -159,22 +151,4 @@ def delete_customer(system_id: int, db: Session):
         )
     db.delete(db_customer)
     db.commit()
-
-    db_customer_hist = model.CustomerHist(
-        customer_id = db_customer.customer_id,
-        first_name = db_customer.first_name,
-        last_name = db_customer.last_name,
-        birth_date = db_customer.birth_date,
-        address = db_customer.address,
-        phone_number = db_customer.phone_number,
-        email = db_customer.email,
-        job_title = db_customer.job_title,
-        active_date = db_customer.active_date,
-        inactive_date = db_customer.inactive_date,
-        created_datetime = db_customer.created_datetime,
-        updated_datetime = current_systime()
-    )
-    db.add(db_customer_hist)
-    db.commit()
-
     return db_customer
