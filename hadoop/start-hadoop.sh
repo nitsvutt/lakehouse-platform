9ffc3979d0605c86575e851215a3cf0929c023e2
nitsvutt <nitsvutt@gmail.com>
2024-07-21 17:45:05 +0700

    set up hadoop

---
+++ hadoop/start-hadoop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$HADOOP_MODE" = "namenode" ]; then
+    $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode --no-prompt -format
+    $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
+
+elif [ "$HADOOP_MODE" = "datanode" ]; then
+    $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
+
+elif [ "$HADOOP_MODE" = "resourcemanager" ]; then
+    $HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR resourcemanager
+
+elif [ "$HADOOP_MODE" = "nodemanager" ]; then
+    $HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR nodemanager
+
+elif [ "$HADOOP_MODE" = "timelineserver" ]; then
+    $HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR timelineserver
+
+else
+    echo "Undefined Mode $HADOOP_MODE, must specify: namenode, datanode, resourcemanager, nodemanager, timelineserver"
+
+fi
