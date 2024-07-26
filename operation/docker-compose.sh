baf23f5d96e49fa59b1144408edf0373e4f7530d
nitsvutt <nitsvutt@gmail.com>
2024-07-26 18:13:19 +0700

    docker-compose.sh

---
+++ operation/docker-compose.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# determine which prompt
+if [ $1 == "up" ]; then
+    command="docker compose up -d"
+elif [ $1 == "down" ]; then
+    command="docker compose down"
+elif [ $1 == "start" ]; then
+    command="docker compose start"
+elif [ $1 == "stop" ]; then
+    command="docker compose stop"
+elif [ $1 == "restart" ]; then
+    command="docker compose restart"
+else
+    echo "ERROR: Invalid prompt $1, must specify: up, down, start, stop, restart"
+    exit 1
+fi
+
+# remove the first argument
+prompt="$1"
+shift
+
+# run prompt with specified services
+available_services=("product1" "product2" "kafka" "hadoop" "hive" "spark" "airflow" "gateway")
+data_source_services=("product1" "product2")
+if [ $1 == "all" ]; then
+    echo "docker compose $prompt all services"
+else
+    for request_service in $@; do
+        if [[ ${available_services[@]} =~ $request_service ]]; then
+            for available_service in ${available_services[@]}; do
+                if [ $request_service == $available_service ]; then
+                    echo "docker compose $prompt $request_service"
+                    if [[  ${data_source_services[@]} =~ $request_service ]]; then
+                        cd "$PROJECT_PATH/iceberg-lakehouse-platform/data-source/$request_service"
+                    else
+                        cd "$PROJECT_PATH/iceberg-lakehouse-platform/$request_service"
+                    fi
+                    eval $command
+                fi
+            done
+        else
+            echo "WARNNING: Undefined service $request_service"
+        fi
+    done
+fi
