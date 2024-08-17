1ecb45e311a21ac60593459c986c5ef970531def
nitsvutt <nitsvutt@gmail.com>
2024-08-17 14:55:46 +0700

    change folder

--- workspace/deployment/notebooks/test_hadoop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-$HADOOP_HOME/bin/hdfs dfs -mkdir hdfs://namenode:9000/user/hive/spark_jars
-
-$HADOOP_HOME/bin/hdfs dfs -put /project/deployment/spark_jars/*.jar hdfs://namenode:9000/user/hive/spark_jars
-
-$HADOOP_HOME/bin/hdfs dfs -ls hdfs://namenode:9000/user/hive/spark_jars
