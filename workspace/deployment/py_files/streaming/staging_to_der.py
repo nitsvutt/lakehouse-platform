866cdd093d2ae6e155c10262e31b6bfe4910617f
nitsvutt <nitsvutt@gmail.com>
2025-06-22 13:11:14 +0700

    complete streaming test

--- workspace/deployment/py_files/streaming/staging_to_der.py
+++ workspace/deployment/py_files/streaming/staging_to_der.py
@@ -1,20 +1,17 @@
-# import libraries
+print(f"{'#'*4} - Import libraries")
 import argparse
 from pyspark.sql import SparkSession
 import re
-from subprocess import Popen, PIPE
 
-# extract parameters
+print(f"{'#'*4} - Extract parameters")
 parser = argparse.ArgumentParser()
 parser.add_argument('--staging_path', type=str)
 parser.add_argument('--data_source', type=str)
 args = parser.parse_args()
 staging_path = args.staging_path
 data_source = args.data_source
-# staging_path = "/user/hive/warehouse/staging"
-# data_source = "product1.public"
 
-# init spark session
+print(f"{'#'*4} - Init spark session")
 spark = (
     SparkSession.builder
     .config("hive.metastore.uris", "thrift://hive-metastore:9083")
@@ -25,11 +22,15 @@ spark = (
     .getOrCreate()
 )
 
-# create new staging tables
+print(f"{'#'*4} - Create new staging tables")
 pattern = re.compile(rf"{data_source}.*")
-p = Popen(f"hdfs dfs -ls {staging_path}/topics", shell=True, stdout=PIPE, stderr=PIPE)
-all_paths = [path.decode("utf-8").split()[-1].split("/")[-1] for path in p.stdout][1:]
+hadoop = spark.sparkContext._jvm.org.apache.hadoop
+fs = hadoop.fs.FileSystem
+conf = hadoop.conf.Configuration() 
+path = hadoop.fs.Path(f"{staging_path}/topics")
+all_paths = [str(f.getPath()).split("/")[-1] for f in fs.get(conf).listStatus(path)]
 all_tables = [path for path in all_paths if pattern.match(path)]
+print(f"{'#'*2} all_tables={all_tables}")
 for table in all_tables:
     hdfs_path = f"hdfs://yarn-master:9000{staging_path}/topics/{table}"
     table_name = table.replace(".", "_")
@@ -39,7 +40,7 @@ for table in all_tables:
         .load(hdfs_path)
     )
     if spark.sql(f"show tables from staging like '{table_name}'").isEmpty():
-        print(table_name)
+        print(f"{'#'*2} create table staging.{table_name}")
         spark.sql(f"""
             create external table if not exists staging.{table_name}
             ({', '.join([col + ' ' + dtype for col, dtype in df.dtypes])})
@@ -49,7 +50,7 @@ for table in all_tables:
         """)
         spark.sql(f'repair table staging.{table_name}')
 
-# create new rawvault tables
+print(f"{'#'*4} - Create new rawvault tables")
 table_name_pattern = data_source.replace(".", "_")
 staging_table_list = (
     spark.sql(f"show tables from staging like '{table_name_pattern}_*'")
@@ -65,14 +66,14 @@ new_tables = list(set(staging_table_list) - set(rawvault_table_list))
 for table in new_tables:
     tables = {"derived": f"{table}_der", "snapshot": f"{table}_snp", "main": table}
     for table_type, table_name in tables.items():
-        print(table_name)
+        print(f"{'#'*2} create table rawvault.{table_name}")
         df = spark.sql(f"select * from staging.{tables['main']}")
         spark.sql(f"""
         create external table if not exists rawvault.{table_name}
         ({', '.join([col + ' ' + dtype for col, dtype in df.dtypes])})
         using iceberg
         {'partitioned by (days(updated_datetime))' if table_type == 'main' else ''}
-        location 'hdfs://yarn-master:9000/user/hive/warehouse/rawvault'
+        location 'hdfs://yarn-master:9000/user/hive/warehouse/rawvault/{table_name}'
         tblproperties(
             'objcapabilities'='extread,extwrite',
             'engine.hive.enabled'='true',
@@ -87,12 +88,12 @@ for table in new_tables:
         )
         """)
 
-# run streaming
+print(f"{'#'*4} - Run streaming")
 for staging_table in staging_table_list:
     stream_reader = (
         spark.readStream
         .schema(spark.sql(f"select * from staging.{staging_table}").schema)
-        .parquet(f"{staging_path}/topics/{staging_table.replace('_', '.')}")
+        .parquet(f"hdfs://yarn-master:9000{staging_path}/topics/{staging_table.replace('_', '.')}")
     )
     stream_query = (
         stream_reader.writeStream
