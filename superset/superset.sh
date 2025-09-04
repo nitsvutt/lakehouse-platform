e68dfb287f01512e42abf93ab6005d1553399128
nitsvutt <nitsvutt@gmail.com>
2025-09-04 23:51:15 +0700

    add network for docker-compose file

--- superset/superset.sh
+++ superset/superset.sh
@@ -8,6 +8,10 @@ else
     echo "sqlalchemy-trino" >> ./superset/docker/requirements-local.txt
 fi
 
+# copy custom file to superset repo
+cp -f ./docker-compose-non-dev.yml ./superset/docker-compose-non-dev.yml
+cp -f ./.env ./superset/docker/.env
+
 # run docker compose if you don't perform any customization on Docker image
 docker-compose -f ./superset/docker-compose-non-dev.yml up -d
 
