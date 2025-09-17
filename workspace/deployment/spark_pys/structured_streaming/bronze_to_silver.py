c82eb69443813bb2d5ed2ec633121d289b9632eb
nitsvutt <nitsvutt@gmail.com>
2025-09-17 17:06:15 +0700

    renew bronze to silver job

--- workspace/deployment/spark_pys/structured_streaming/bronze_to_silver.py
+++ workspace/deployment/spark_pys/structured_streaming/bronze_to_silver.py
@@ -1,15 +1,18 @@
 print(f"{'#'*4} - Import libraries")
 import argparse
+import json
 from pyspark.sql import SparkSession
 import re
 
 print(f"{'#'*4} - Extract parameters")
 parser = argparse.ArgumentParser()
-parser.add_argument('--staging_path', type=str)
+parser.add_argument('--warehouse_path', type=str)
 parser.add_argument('--data_source', type=str)
+parser.add_argument('--biz_key_map', type=str)
 args = parser.parse_args()
-staging_path = args.staging_path
+warehouse_path = args.warehouse_path
 data_source = args.data_source
+biz_key_map = json.loads(args.biz_key_map)
 
 print(f"{'#'*4} - Init spark session")
 spark = (
@@ -22,73 +25,88 @@ spark = (
     .getOrCreate()
 )
 
-print(f"{'#'*4} - Create new staging tables")
+print(f"{'#'*2} - Create new bronze tables")
 pattern = re.compile(rf"{data_source}.*")
 hadoop = spark.sparkContext._jvm.org.apache.hadoop
 fs = hadoop.fs.FileSystem
 conf = hadoop.conf.Configuration() 
-path = hadoop.fs.Path(f"{staging_path}/topics")
+path = hadoop.fs.Path(f"{warehouse_path}/bronze/topics")
 all_paths = [str(f.getPath()).split("/")[-1] for f in fs.get(conf).listStatus(path)]
 all_tables = [path for path in all_paths if pattern.match(path)]
-print(f"{'#'*2} all_tables={all_tables}")
+print(f"{'#'*4} - all_tables={all_tables}")
 for table in all_tables:
-    hdfs_path = f"hdfs://yarn-master:9000{staging_path}/topics/{table}"
+    hdfs_path = f"{warehouse_path}/bronze/topics/{table}"
     table_name = table.replace(".", "_")
     df = (
         spark.read.format('parquet')
         .options(header=True, inferSchema=True)
         .load(hdfs_path)
     )
-    if spark.sql(f"show tables from staging like '{table_name}'").isEmpty():
-        print(f"{'#'*2} create table staging.{table_name}")
+    if spark.sql(f"show tables from bronze like '{table_name}'").isEmpty():
+        print(f"{'#'*4} - Create table bronze.{table_name}")
         spark.sql(f"""
-            create external table if not exists staging.{table_name}
+            create external table if not exists bronze.{table_name}
             ({', '.join([col + ' ' + dtype for col, dtype in df.dtypes])})
             partitioned by (__ds)
             stored as parquet
             location "{hdfs_path}"
         """)
-        spark.sql(f'repair table staging.{table_name}')
+        spark.sql(f'repair table bronze.{table_name}')
 
-print(f"{'#'*4} - Create new rawvault tables")
+print(f"{'#'*2} - Create new silver tables")
 table_name_pattern = data_source.replace(".", "_")
-staging_table_list = (
-    spark.sql(f"show tables from staging like '{table_name_pattern}_*'")
+bronze_table_list = (
+    spark.sql(f"show tables from bronze like '{table_name_pattern}_*'")
         .select("tableName")
         .rdd.flatMap(lambda x: x).collect()
 )
-rawvault_table_list = (
-    spark.sql(f"show tables from rawvault like '{table_name_pattern}_*'")
+silver_table_list = (
+    spark.sql(f"show tables from silver like '{table_name_pattern}_*'")
         .select("tableName")
         .rdd.flatMap(lambda x: x).collect()
 )
-new_tables = list(set(staging_table_list) - set(rawvault_table_list))
-for table in new_tables:
-    tables = {"derived": f"{table}_der", "snapshot": f"{table}_snp", "main": table}
-    for table_type, table_name in tables.items():
-        print(f"{'#'*2} create table rawvault.{table_name}")
-        df = spark.sql(f"select * from staging.{tables['main']}")
+new_bronze_tables = list(set(bronze_table_list) - set(silver_table_list))
+print(f"{'#'*4} - new_bronze_tables={new_bronze_tables}")
+for new_bronze_table in new_bronze_tables:
+    table_list = {"derived": f"{new_bronze_table}_der", "snapshot": f"{new_bronze_table}_snp"}
+    df = spark.sql(f"select * from bronze.{new_bronze_table}")
+    for table_type, table_name in table_list.items():
+        print(f"{'#'*4} - Create table silver.{table_name}")
         spark.sql(f"""
-        create external table if not exists rawvault.{table_name}
-        ({', '.join([col + ' ' + dtype for col, dtype in df.dtypes])})
-        using iceberg
-        {'partitioned by (days(updated_datetime))' if table_type == 'main' else ''}
-        location 'hdfs:///user/hive/warehouse/rawvault/{table_name}'
-        tblproperties(
-            'objcapabilities'='extread,extwrite',
-            'engine.hive.enabled'='true',
-            'write.delete.mode'='copy-on-write',
-            'write.update.mode'='copy-on-write',
-            'write.merge.mode'='copy-on-write',
-            'external.table.purge'='true',
-            'iceberg.file_format'='parquet',
-            'format-version'='2',
-            'read.parquet.vectorization.batch-size'='10000',
-            'read.parquet.vectorization.enabled'='false'
-        )
+            create external table if not exists silver.{table_name}
+            ({', '.join([col + ' ' + dtype for col, dtype in df.dtypes])})
+            using iceberg
+            location '{warehouse_path}/silver/{table_name}'
+            tblproperties(
+                'objcapabilities'='extread,extwrite',
+                'engine.hive.enabled'='true',
+                'write.delete.mode'='copy-on-write',
+                'write.update.mode'='copy-on-write',
+                'write.merge.mode'='copy-on-write',
+                'external.table.purge'='true',
+                'iceberg.file_format'='parquet',
+                'format-version'='2',
+                'read.parquet.vectorization.batch-size'='10000',
+                'read.parquet.vectorization.enabled'='false'
+            )
         """)
+    print(f"{'#'*4} - Create view silver.{new_bronze_table}")
+    spark.sql(f"""
+        create view if not exists silver.{new_bronze_table} as
+        with ranking as (
+            select *,
+                row_number() over(
+                    partition by {', '.join(biz_key_map[new_bronze_table.replace(f'{table_name_pattern}_', '')])}
+                    order by __src_ts_ms desc, __lsn desc
+                ) as rn
+            from silver.{table_list['derived']}
+        )
+        select *
+        from ranking
+        where rn = 1 and __op <> 'd'
+    """)
 
-print(f"{'#'*4} - Run streaming")
+print(f"{'#'*2} - Run streaming")
 for staging_table in staging_table_list:
     stream_reader = (
         spark.readStream
